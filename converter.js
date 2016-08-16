d849427556a3c84d508fe97e6d82a92be44c76d6
reallyimeric <reallyimeric@gmail.com>
2016-08-17 01:42:18 +0800

    corrected proposal data, modified convertor
    corrected name of prize in proposal data
    convertor will not throw exception any more when input file not found
    convertor will accept another input filename
    added an output when starting

--- converter.js
+++ converter.js
@@ -1,45 +1,60 @@
 'use strict'
 const xlsx = require('xlsx')
 const sqlite3 = require('sqlite3')
-const source = xlsx.readFile('./數據資料表.xls')
+const fs = require('fs')
+let fileName = './items.xls'
+try {
+  let stats = fs.statSync(fileName)
+  if (!stats.isFile()) {throw 'not file'}
+} catch(err) {
+  try {
+    fileName = '數據資料表.xls'
+    let stats = fs.statSync(fileName)
+    if (!stats.isFile()) {throw 'not file'}
+  } catch(err) {
+    console.error('File "items.xls"/"數據資料表.xls" not existed?')
+    process.exit(1)
+  }
+}
+
+const source = xlsx.readFile(fileName)
 const sheet0 = source.Sheets[source.SheetNames[0]]
-const target = new sqlite3.Database('./items.sqlite', common_error_handler)
+const target = new sqlite3.Database('./items.sqlite', commonErrorHandler)
 const tableName = 'items'
 
 target.serialize()
-target.run('PRAGMA journal_mode = OFF', common_error_handler)
+target.run('PRAGMA journal_mode = OFF', commonErrorHandler)
 target.run(`CREATE TABLE "main"."${tableName}" (
   "id" INTEGER UNIQUE ,
   "name" TEXT ,
   "price" REAL ,
   "time" INTEGER DEFAULT 0)
   `, err => {
-  if ( err ){
-    if (err.errno == 1){ //or .....all error's errno are 1?
-      console.log(`does the table already exist?`)
-      console.log(`halt`)
-      process.exit(1)
-    }
+  if ( err ) {
+    console.err('does the table already exist?')
+    console.err('halt')
+    process.exit(1)
   }
 })
 const statement = target.prepare(`INSERT INTO ${tableName} (id, name) VALUES (?1, ?2)`)
 target.parallelize()
+console.log('Start...')
 const timestamp = new Date().getTime()
 for (let index = 2; index != 17526 + 1 ; index++)
 {
   let id = sheet0[`A${index}`].v
   let name
   try{name = sheet0[`B${index}`].v}catch(err){name = ''}
-  statement.run(id, name, common_error_handler)
+  statement.run(id, name, commonErrorHandler)
   // console.log(`${index} -- ${name} = ${id}`)
 }
 target.serialize()
 statement.finalize()
 target.close(() => console.log(`Database time cost: ${(new Date().getTime() - timestamp)/1000}s`))
 
-function common_error_handler(err){
-  if (err != null ) {
-    console.log(`${err}`)
+function commonErrorHandler(err){
+  if (err) {
+    console.error(`${err}`)
     process.exit(1)
   }
 }
